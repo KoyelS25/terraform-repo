917787ff6d503abcb6fb89d727c4258c913484e2
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-04 21:23:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 
   region = "ap-south-1"
-  access_key = "AKIAXKIZZIZ7UJNURRGC"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   }
