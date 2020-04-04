0893f87d1db025fd767ef5295ea569c198edac68
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-04 15:04:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "test-easy-learn-terraform"
   acl = "private"
 
-  tags {
-    Name = "Terraform Bucket"
-    Environment = "Dev-Env"
-  }
+
   versioning {
     enabled = true
   }
