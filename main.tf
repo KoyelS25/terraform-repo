7bc4552c0d6b15463f1d977aa8e58cd5785b070c
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-04 15:14:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
 
   region = "ap-south-1"
-  access_key = "AKIAJPMPPAYZS5LN4TVQ"
-  secret_key = "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"
+  access_key = "AKIAXKIZZIZ72UMEDO7R"
+  secret_key = "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"
   }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "test-easy-learn-terraform"
+  bucket = "test-terraform"
   acl = "private"
 
 
