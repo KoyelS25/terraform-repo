b5f82a497b921c67b7ee0e1ac2876441826f07a8
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-04 20:49:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,20 @@ resource "aws_iam_role_policy" "ec2_policy" {
   name = "ec2_policy"
   role = "aws_iam_role.terraform_ec2_role.id"
 
-  policy = "${file("ec2_policy.json")}"
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "ec2:*"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
 
 resource "aws_iam_role" "terraform_ec2_role" {
