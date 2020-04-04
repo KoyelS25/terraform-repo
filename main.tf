653b64ae514fdfdde8bc909b770753a0d5804bee
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-04 21:00:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,42 +5,23 @@ provider "aws" {
   secret_key = "TZTYJJGGs4McbrLqp+x3NAQVZjpZkE1jvQ9YBZ60"
   }
 
-resource "aws_iam_role_policy" "ec2_policy" {
-  name = "ec2_policy_first"
-  role = "aws_iam_role.terraform_ec2_role.id"
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
 
   policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "ec2:*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_role" "terraform_ec2_role" {
-    name = "ec2_policy_role"
-    assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            },
-
-            "Effect": "Allow",
-            "Sid": ""
-        }
-    ]
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
 }
 EOF
 }
