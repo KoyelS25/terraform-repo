b22b13aaa004002bca18289765a1c8181af24a24
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-04 21:10:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,24 +4,3 @@ provider "aws" {
   access_key = "AKIAXKIZZIZ7UJNURRGC"
   secret_key = "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"
   }
-
-resource "aws_iam_policy" "policy" {
-  name        = "test_policy"
-  path        = "/"
-  description = "My test policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
