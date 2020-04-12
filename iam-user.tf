ea731574091fd858ba8c11d30597951cda990634
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-12 13:59:12 +0530

    Update iam-user.tf

--- iam-user.tf
+++ iam-user.tf
@@ -51,5 +51,5 @@ resource "aws_iam_policy_attachment" "test-attach" {
   users      = ["${aws_iam_user.user.name}"]
   roles      = ["${aws_iam_role.role.name}"]
   groups     = ["${aws_iam_group.group.name}"]
-  policy_arn = "${aws_iam_policy.policy.arn}"
+  policy_arn = ["${aws_iam_policy.policy.arn}"]
 }
