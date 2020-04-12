8c61ec2601bf015ed36874df856f7f0d254a095b
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-12 13:55:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,34 @@
-provider "aws" {
+resource "aws_kms_key" "a" {}
 
-  region = "ap-south-1"
-  access_key = ""
-  secret_key = ""
+resource "aws_iam_role" "a" {
+  name = "iam-role-for-grant"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_kms_grant" "a" {
+  name              = "my-grant"
+  key_id            = "${aws_kms_key.a.key_id}"
+  grantee_principal = "${aws_iam_role.a.arn}"
+  operations        = ["Encrypt", "Decrypt", "GenerateDataKey"]
+
+  constraints {
+    encryption_context_equals = {
+      Department = "Finance"
+    }
   }
+}
