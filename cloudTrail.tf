7e50ae1553200db8d3240c85f7032a78d173f05a
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 18:19:28 +0530

    Update cloudTrail.tf

--- cloudTrail.tf
+++ cloudTrail.tf
@@ -4,35 +4,3 @@ resource "aws_cloudtrail" "ct_log" {
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AWSCloudTrailAclCheck",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:GetBucketAcl",
-            "Resource": "arn:aws:s3:::tf-test-trail"
-        },
-        {
-            "Sid": "AWSCloudTrailWrite",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::tf-test-trail/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:x-amz-acl": "bucket-owner-full-control"
-                }
-            }
-        }
-    ]
-}
-EOF
-}
