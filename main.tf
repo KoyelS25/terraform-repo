ef82b58b8926f023b0635d4036d7bdededed794d
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 23:15:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,9 +30,3 @@ resource "aws_subnet" "main" {
     Name = "Main"
   }
 }
-
-resource "aws_vpc_peering_connection" "foo" {
-  peer_owner_id = "503102129791"
-  peer_vpc_id   = "vpc-0e3616a4c61601122"
-  vpc_id        = "vpc-f44b108f"
-}
