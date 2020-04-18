a179301aaa2140e94427685372929431c2f9ecb2
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-19 00:51:26 +0530

    Update iam_user.tf

--- iam_user.tf
+++ iam_user.tf
@@ -6,27 +6,3 @@ resource "aws_iam_user" "new_book" {
     tag-key = "tag-value"
   }
 }
-
-resource "aws_iam_access_key" "new_book" {
-  user = "new_page"
-}
-
-resource "aws_iam_user_policy" "pro_policy" {
-  name = "test"
-  user = "new_page"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
