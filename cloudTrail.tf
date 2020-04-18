bc92f3e62c3c995a14d3d17d8dbd4834f8461d77
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 17:02:13 +0530

    Create cloudTrail.tf

---
+++ cloudTrail.tf
@@ -0,0 +1,38 @@
+resource "aws_cloudtrail" "ct_log" {
+  name                          = "tf-trail-ct_log"
+  s3_bucket_name                = "sumana"
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+}
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::tf-test-trail"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::tf-test-trail/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
