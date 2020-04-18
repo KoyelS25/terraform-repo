cbd92cb315695fc33adb87c61a5d12d1d2a68074
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-19 00:21:08 +0530

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,17 @@
+resource "aws_default_security_group" "sg_one" {
+  vpc_id = "vpc-0d3edbc350701377a"
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
