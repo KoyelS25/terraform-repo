48fa15a926116492610e8e43c0927c1ce18c55f8
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 19:39:10 +0530

    Update cloudTrail.tf

--- cloudTrail.tf
+++ cloudTrail.tf
@@ -1,13 +1,13 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_cloudtrail" "ct_log" {
-  name                          = "tf-trail-foobar"
+  name                          = "ct_log"
   s3_bucket_name                = "terra-cloudtrail"
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket" "foo" {
+resource "aws_s3_bucket" "terra-cloudtrail" {
   bucket        = "terra-cloudtrail"
   force_destroy = true
 
