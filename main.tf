89bbd6685f46010b9b48ebc76112424065b98983
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 23:20:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,17 +8,6 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-
-resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "terraform-example-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  tags = {
-    Environment = "production"
-  }
-}
 resource "aws_subnet" "main" {
   vpc_id     = "vpc-0e3616a4c61601122"
   cidr_block = "10.10.0.0/24"
