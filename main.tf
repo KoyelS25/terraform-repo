0802ec0c429234cbaac09a5ce9a9cc9ff6bc2751
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 23:22:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,3 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = "vpc-0e3616a4c61601122"
-  cidr_block = "10.10.0.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}
