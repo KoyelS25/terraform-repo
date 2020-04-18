e28277ea9d7138ca58964cd81cd08b8effde76c9
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 19:41:20 +0530

    Update cloudTrail.tf

--- cloudTrail.tf
+++ cloudTrail.tf
@@ -1,14 +1,14 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_cloudtrail" "ct_log" {
-  name                          = "ct_log"
-  s3_bucket_name                = "terra-cloudtrail"
+resource "aws_cloudtrail" "foobar" {
+  name                          = "tf-trail-foobar"
+  s3_bucket_name                = "${aws_s3_bucket.foo.id}"
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket" "terra-cloudtrail" {
-  bucket        = "terra-cloudtrail"
+resource "aws_s3_bucket" "foo" {
+  bucket        = "tf-test-trail"
   force_destroy = true
 
   policy = <<POLICY
