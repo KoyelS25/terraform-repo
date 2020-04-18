2a0458960b7ae40a0cd327c77eccf86f27cd438e
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 19:35:42 +0530

    Update cloudTrail.tf

--- cloudTrail.tf
+++ cloudTrail.tf
@@ -1,5 +1,42 @@
 resource "aws_cloudtrail" "ct_log" {
-  name                          = "tf-trail-ct_log"
+  name                          = "tf-trail-foobar"
   s3_bucket_name                = "terra-cloudtrail"
+  s3_key_prefix                 = "prefix"
   include_global_service_events = false
+}
+
+resource "aws_s3_bucket" "foo" {
+  bucket        = "terra-cloudtrail"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::tf-test-trail"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::tf-test-trail/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
 }
