c137af3693ac1a14d4fe17d8078a43c3de5da13a
Sumana29 <44775218+Sumana29@users.noreply.github.com>
2020-04-18 18:50:01 +0530

    Update cloudTrail.tf

--- cloudTrail.tf
+++ cloudTrail.tf
@@ -1,6 +1,5 @@
 resource "aws_cloudtrail" "ct_log" {
   name                          = "tf-trail-ct_log"
   s3_bucket_name                = "sumana"
-  s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
